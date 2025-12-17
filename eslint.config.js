9ee358d3d7d53d8091bb5cbb7110caa13f2d7940
Winterest | M. Adam Yudistira <yudistiraadam3@gmail.com>
2025-12-17 21:17:29 +0700

    config: install dan konfigurasi plugin eslint untuk react

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,9 @@ import eslintPluginAstro from "eslint-plugin-astro";
 import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
 import eslintPluginPrettier from "eslint-plugin-prettier";
+import eslintPluginReact from "eslint-plugin-react";
+import eslintPluginReactHooks from "eslint-plugin-react-hooks";
+import globals from "globals";
 
 export default [
   ...tseslint.configs.recommended,
@@ -14,6 +17,49 @@ export default [
         parser: "@typescript-eslint/parser",
         extraFileExtensions: [".astro"],
       },
+    },
+  },
+  // React configuration
+  {
+    files: ["**/*.{jsx,tsx}"],
+    plugins: {
+      react: eslintPluginReact,
+      "react-hooks": eslintPluginReactHooks,
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      // React rules
+      "react/react-in-jsx-scope": "off", // Not needed in React 17+
+      "react/prop-types": "off", // Using TypeScript
+      "react/jsx-uses-react": "off",
+      "react/jsx-uses-vars": "error",
+      "react/jsx-key": "warn",
+      "react/jsx-no-duplicate-props": "error",
+      "react/jsx-no-undef": "error",
+      "react/no-children-prop": "warn",
+      "react/no-danger-with-children": "error",
+      "react/no-deprecated": "warn",
+      "react/no-direct-mutation-state": "error",
+      "react/no-unescaped-entities": "warn",
+      "react/no-unknown-property": "error",
+      "react/self-closing-comp": "warn",
+      // React Hooks rules
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
     },
   },
   {
