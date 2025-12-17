e87e5960fc7f673aa8003064c97511ac0d51b37f
Winterest | M. Adam Yudistira <yudistiraadam3@gmail.com>
2025-12-17 21:00:21 +0700

    config: install and configure prettier plugin to integrated with eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,12 @@
 import eslintPluginAstro from "eslint-plugin-astro";
 import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettier from "eslint-plugin-prettier";
 
 export default [
   ...tseslint.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
+  eslintConfigPrettier,
   {
     files: ["**/*.astro"],
     languageOptions: {
@@ -14,8 +17,13 @@ export default [
     },
   },
   {
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
     rules: {
-      // override/add rules settings here
+      // Prettier integration - show formatting issues as warnings
+      "prettier/prettier": "warn",
+      // TypeScript rules
       "@typescript-eslint/no-unused-vars": "warn",
     },
   },
